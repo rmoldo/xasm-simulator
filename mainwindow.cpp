7ed6693fa52d851660b41103c2c190ee34c714ae
Radu Moldovan <radu.moldovan@ulbsibiu.ro>
2021-05-03 17:57:48 +0300

    Added memory viewer to main ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    memoryViewerDialog = new MemoryViewerDialog(this);
+
     createActions();
 }
 
@@ -39,4 +42,22 @@ void MainWindow::createActions() {
     quitAction->setStatusTip(tr("Quit XASM Simulator"));
     connect(quitAction, &QAction::triggered, qApp, &QApplication::closeAllWindows, Qt::QueuedConnection);
     fileMenu->addAction(quitAction);
+
+    QMenu *viewMenu = menuBar()->addMenu(tr("&View"));
+
+    QAction *viewMemoryAction = new QAction(tr("&View Memory"), this);
+    viewMemoryAction->setIcon(QPixmap(":/rec/resources/icons/ram.svg"));
+    viewMemoryAction->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_M));
+    viewMemoryAction->setStatusTip(tr("View memory contents"));
+
+    connect(viewMemoryAction, &QAction::triggered, this->memoryViewerDialog,
+            [this]() {
+        // TOOD(Moldo) change this when we implement the processor
+        unsigned char test[20] = {0x12, 0x10, 0x74, 0x44, 0x66, 0x43, 0x24, 0x76, 0x99, 0x56, 0x44, 0x22, 0x10, 0x12, 0x14, 0x24, 0x66};
+        memoryViewerDialog->setMemoryViewerData(reinterpret_cast<char*>(test));
+        memoryViewerDialog->show();
+    });
+
+    viewMenu->addAction(viewMemoryAction);
+    fileToolBar->addAction(viewMemoryAction);
 }
