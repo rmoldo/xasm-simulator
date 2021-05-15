cdb0ed67d35b76865d98d0257f79eeff36e073d8
Radu Moldovan <radu.moldovan@ulbsibiu.ro>
2021-05-15 13:34:04 +0300

    PR changes
    Reinitialised the memory when you set the machine code into memory

--- cpu.cpp
+++ cpu.cpp
@@ -75,6 +75,7 @@ std::vector<u8> Cpu::getMemory() {
 }
 
 void Cpu::setMachineCodeInMemory(u8 *data, size_t size) {
+    std::fill(memory.begin(), memory.end(), 0x0);
     memory.erase(memory.begin(), memory.begin() + size);
     memory.insert(memory.begin(), data, data + size);
 }
